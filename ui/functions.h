cac4efbee9062a48c65d130780ba6a6cedf2e5c2
Xurape <xarope1337@gmail.com>
2022-11-04 18:42:52 +0000

    Created a new function

---
+++ ui/functions.h
@@ -0,0 +1,14 @@
+/*
+|--------------------------------------------------------------------------
+| Gerar um menu
+|--------------------------------------------------------------------------
+|
+| Esta função irá gerar um menu consoante
+| o número de colunas e linhas dadas pela
+| função quando utilizada.
+|
+*/
+void gerarMenu()
+{
+    
+}
