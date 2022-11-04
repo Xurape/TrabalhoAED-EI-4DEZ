418c539705913ffd5a656947273f42ddc3bf2d12
Xurape <xarope1337@gmail.com>
2022-11-04 23:21:45 +0000

    Added initial options

---
+++ ui/options.h
@@ -0,0 +1,10 @@
+#define NUMERO_DE_OPCOES_INICIAIS 4
+#define NUMERO_MAX_DE_CARACTERES 64
+
+const char opcoesIniciais[NUMERO_DE_OPCOES_INICIAIS][NUMERO_MAX_DE_CARACTERES] = 
+{
+    "Equipamentos", 
+    "Aplicações", 
+    "Placas de rede", 
+    "Listar items"
+};
