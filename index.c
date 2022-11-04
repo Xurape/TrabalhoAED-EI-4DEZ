e3fd11f18c12aa02dcf09739aa2496c375c2cea0
Xurape <xarope1337@gmail.com>
2022-11-04 14:35:54 +0000

    Included everything

--- index.c
+++ index.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <curses.h>
+
+// Database
+#include "./database/data.h"
 
 void main (int argc, char *argv[]) {
     printf("Hello world!\n");
