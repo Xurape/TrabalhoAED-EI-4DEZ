16a0ee4265097cbd6e7b911252c8efae513b8fcb
Xurape <xarope1337@gmail.com>
2022-11-04 23:21:30 +0000

    Added new UI functions

--- ui/functions.h
+++ ui/functions.h
@@ -1,14 +1,71 @@
+// Incluir as cores
+#include "../utils/colors.h"
+
+// Incluir opções dos menus
+#include "./options.h"
+
 /*
 |--------------------------------------------------------------------------
-| Gerar um menu
+| Configurações iniciais do programa
 |--------------------------------------------------------------------------
 |
-| Esta função irá gerar um menu consoante
-| o número de colunas e linhas dadas pela
-| função quando utilizada.
+| Esta função irá correr a configuração inicial do programa
 |
 */
-void gerarMenu()
+void configuracaoInicial()
 {
-    
+    setlocale(LC_ALL, "Portuguese");
 }
+
+/*
+|--------------------------------------------------------------------------
+| Limpar a janela
+|--------------------------------------------------------------------------
+|
+| Esta função irá limpar a janela ao utilizador dependendo
+| do sistema operativo a ser usado
+|
+*/
+void limparJanela()
+{
+    if (obterSO() == "Linux")
+        system("clear");
+    else if (obterSO() == "Windows")
+        system("cls");
+    else if (obterSO() == "MacOS")
+        system("clear");
+    else
+        system("clear");
+}
+
+/*
+|--------------------------------------------------------------------------
+| Quebra a quantidade de linhas indicada
+|--------------------------------------------------------------------------
+|
+| Esta função irá mudar/quebrar as linhas indicadas
+|
+*/
+void quebrarLinhas(int linhas)
+{
+    for (size_t i = 1; i <= linhas; i++)
+    {
+        printf("\n");
+    }   
+}
+
+/*
+|--------------------------------------------------------------------------
+| Finalizar menu
+|--------------------------------------------------------------------------
+|
+| Esta função irá finalizar os menus
+|
+*/
+void finalizarMenu()
+{
+    printf("\n\n");
+    exit(0);
+}
+
+// printf("Sistema operativo detetado: %s\n", obterSO());
