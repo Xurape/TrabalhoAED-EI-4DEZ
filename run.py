843b42b07f7c2ec789bb8a5b0272eaafd5009c52
Xurape <xarope1337@gmail.com>
2022-12-06 14:16:26 +0000

    Added a just run it in python

--- run.py
+++ run.py
@@ -4,21 +4,7 @@ import time
 
 # Compile func
 def compile():
-    if platform.system() == 'Windows':
-        os.system("gcc main.c -o main.exe -w")
-    else:
-        os.system("gcc main.c -o main -w")
-
-# Run func
-def runit():
-    if platform.system() == 'Windows':
-        os.system(".\\main.exe")
-    else:
-        os.system("./main")
+        os.system("gcc main.c -o main -w && ./main")
 
 # Compilar & Correr
 compile()
-
-time.sleep(2)
-
-runit()
