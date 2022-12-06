cac05cda45b92e0c9268d9352281fc971dce34b2
Xurape <xarope1337@gmail.com>
2022-12-06 13:44:22 +0000

    Added space between inputs and added verification

--- ui/menu/equipamentos.c
+++ ui/menu/equipamentos.c
@@ -23,17 +23,22 @@ void equipamentosInserir()
 
     printf(COR_Green"\n  A inserir equipamento com ID: "COR_Cyan"[ %d ]\n\n"COR_Default, id);
 
-    printf(COR_Cyan " ■ Caso seja um computador [1] ou servidor [2] " COR_Green "→ "); scanf("%d", &equipamento[id].tipo); //quebrarLinhas(1);
-    printf(COR_Cyan " ■ Data de aquisição " COR_Green "→ "); scanf("%d/%d/%d", &equipamento[id].aquisicao.dia, &equipamento[id].aquisicao.mes, &equipamento[id].aquisicao.ano); //quebrarLinhas(1);
-    printf(COR_Cyan " ■ Departamento " COR_Green "→ "); fflush(stdin); scanf("%c", &temp); gets(&equipamento[id].departamento); //quebrarLinhas(1);
-    printf(COR_Cyan " ■ Garantia (em meses) " COR_Green "→ "); scanf("%d", &equipamento[id].garantia); //quebrarLinhas(1);
-    printf(COR_Cyan " ■ CPU " COR_Green "→ "); fflush(stdin); scanf("%c", &temp); gets(&equipamento[id].cpus.cpu); //quebrarLinhas(1);
-    printf(COR_Cyan " ■ Frequência de CPU (em GHz) " COR_Green "→ "); scanf("%f", &equipamento[id].cpus.ghz); //quebrarLinhas(1);
-    printf(COR_Cyan " ■ Tipo de disco (SSD/SAS/HDD) " COR_Green "→ "); fflush(stdin); scanf("%c", &temp); gets(&equipamento[id].discos.tipo); //quebrarLinhas(1);
-    printf(COR_Cyan " ■ Nome do disco (Ex.: Kingston <Modelo>) " COR_Green "→ "); fflush(stdin); gets(&equipamento[id].discos.nome); //quebrarLinhas(1); fflush(stdin); 
-    printf(COR_Cyan " ■ Capacidade do disco em GB (Ex.: 500) " COR_Green "→ "); scanf("%d", &equipamento[id].discos.capacidade); //quebrarLinhas(1);
-    printf(COR_Cyan " ■ Quantidade de memória RAM em GB (Ex.: 8) " COR_Green "→ "); scanf("%d", &equipamento[id].ram); //quebrarLinhas(1); fflush(stdin);
-    printf(COR_Cyan " ■ Sistema operativo (Ex.: Windows 11) " COR_Green "→ "); fflush(stdin); scanf("%c", &temp); gets(&equipamento[id].sistemaoperativo); //quebrarLinhas(1);
+    printf(COR_Cyan " ■ Caso seja um computador [1] ou servidor [2] " COR_Green "→ "); scanf("%d", &equipamento[id].tipo);
+    while(equipamento[id].tipo != 1 && equipamento[id].tipo != 2)
+    {
+        printf(COR_Red "\n Por favor, insira um número válido.\n\n");
+        printf(COR_Cyan " ■ Caso seja um computador [1] ou servidor [2] " COR_Green "→ "); scanf("%d", &equipamento[id].tipo);
+    }
+    printf(COR_Cyan " ■ Data de aquisição " COR_Green "→ "); scanf("%d/%d/%d", &equipamento[id].aquisicao.dia, &equipamento[id].aquisicao.mes, &equipamento[id].aquisicao.ano);
+    printf(COR_Cyan " ■ Departamento " COR_Green "→ "); fflush(stdin); scanf("%c", &temp); gets(&equipamento[id].departamento);
+    printf(COR_Cyan " ■ Garantia (em meses) " COR_Green "→ "); scanf("%d", &equipamento[id].garantia);
+    printf(COR_Cyan " ■ CPU " COR_Green "→ "); fflush(stdin); scanf("%c", &temp); gets(&equipamento[id].cpus.cpu);
+    printf(COR_Cyan " ■ Frequência de CPU (em GHz) " COR_Green "→ "); scanf("%f", &equipamento[id].cpus.ghz);
+    printf(COR_Cyan " ■ Tipo de disco (SSD/SAS/HDD) " COR_Green "→ "); fflush(stdin); scanf("%c", &temp); gets(&equipamento[id].discos.tipo);
+    printf(COR_Cyan " ■ Nome do disco (Ex.: Kingston <Modelo>) " COR_Green "→ "); fflush(stdin); gets(&equipamento[id].discos.nome); fflush(stdin); 
+    printf(COR_Cyan " ■ Capacidade do disco em GB (Ex.: 500) " COR_Green "→ "); scanf("%d", &equipamento[id].discos.capacidade);
+    printf(COR_Cyan " ■ Quantidade de memória RAM em GB (Ex.: 8) " COR_Green "→ "); scanf("%d", &equipamento[id].ram); fflush(stdin);
+    printf(COR_Cyan " ■ Sistema operativo (Ex.: Windows 11) " COR_Green "→ "); fflush(stdin); scanf("%c", &temp); gets(&equipamento[id].sistemaoperativo);
 
     equipamentos_id++;
 }
@@ -107,21 +112,21 @@ void equipamentosFiltrar()
 
     printf(COR_Green "  [SELECIONADO]" COR_Cyan " Filtrar equipamentos\n\n");
 
-    printf(COR_Cyan "[1] "COR_Default"Equipamento por Sistema Operativo\n"); // Feito
-    printf(COR_Cyan "[2] "COR_Default"Equipamento por Placa de Rede\n");
-    printf(COR_Cyan "[3] "COR_Default"Equipamento por Aplicação\n");
-    printf(COR_Cyan "[4] "COR_Default"Equipamento por Departamento\n\n");
+    printf(COR_Cyan " [1] "COR_Default"Equipamento por Sistema Operativo\n"); // Feito
+    printf(COR_Cyan " [2] "COR_Default"Equipamento por Placa de Rede\n");
+    printf(COR_Cyan " [3] "COR_Default"Equipamento por Aplicação\n");
+    printf(COR_Cyan " [4] "COR_Default"Equipamento por Departamento\n\n");
 
-    printf(COR_Cyan "[5] "COR_Default"Equipamento por Garantia Expirada\n"); // Feito
-    printf(COR_Cyan "[6] "COR_Default"Equipamento por Número de MIPS (Por departamento)\n");
-    printf(COR_Cyan "[7] "COR_Default"Equipamento por Memória (Por departamento)\n");
-    printf(COR_Cyan "[8] "COR_Default"Equipamento por Capacidade do disco duro (Por departamento)\n");
-    printf(COR_Cyan "[9] "COR_Default"Equipamento com menos de um determinado Nº de RAM\n");
-    printf(COR_Cyan "[10] "COR_Default"Equipamento por Aplicação com validade expirada\n");
-    printf(COR_Cyan "[11] "COR_Default"Equipamentos na mesma rede\n");
-    printf(COR_Cyan "[12] "COR_Default"Verificar se equipamentos com possibilidade de comunicação interna.\n");
+    printf(COR_Cyan " [5] "COR_Default"Equipamento por Garantia Expirada\n"); // Feito
+    printf(COR_Cyan " [6] "COR_Default"Equipamento por Número de MIPS (Por departamento)\n");
+    printf(COR_Cyan " [7] "COR_Default"Equipamento por Memória (Por departamento)\n");
+    printf(COR_Cyan " [8] "COR_Default"Equipamento por Capacidade do disco duro (Por departamento)\n");
+    printf(COR_Cyan " [9] "COR_Default"Equipamento com menos de um determinado Nº de RAM\n");
+    printf(COR_Cyan " [10] "COR_Default"Equipamento por Aplicação com validade expirada\n");
+    printf(COR_Cyan " [11] "COR_Default"Equipamentos na mesma rede\n");
+    printf(COR_Cyan " [12] "COR_Default"Verificar se equipamentos com possibilidade de comunicação interna.\n");
 
-    printf(COR_Cyan"\n■"COR_Default" Filtrar → ");
+    printf(COR_Cyan"\n ■"COR_Default" Filtrar → ");
     scanf("%d", &filtro);
 
     quebrarLinhas(2);
